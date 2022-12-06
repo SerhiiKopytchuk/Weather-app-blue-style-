fc6dd49bb32903d9d4624012c3f0006db7e962e8
Serhii <kopytchukserhii@gmail.com>
2022-12-06 17:22:27 +0200

    added images

---
+++ weatherNatife/weatherNatife/View/HourListRow.swift
@@ -0,0 +1,20 @@
+//
+//  HourListRow.swift
+//  weatherNatife
+//
+//  Created by Serhii Kopytchuk on 06.12.2022.
+//
+
+import SwiftUI
+
+struct HourListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HourListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HourListRow()
+    }
+}
