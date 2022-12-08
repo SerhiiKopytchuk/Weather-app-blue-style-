1c3c0616295ff23d75fd5bda141636bfc3221312
Serhii <kopytchukserhii@gmail.com>
2022-12-08 11:18:52 +0200

    update weatherView model

--- weatherNatife/weatherNatife/ViewModel/WeatherViewModel.swift
+++ weatherNatife/weatherNatife/ViewModel/WeatherViewModel.swift
@@ -7,8 +7,24 @@
 
 import SwiftUI
 import MapKit
+import Combine
 
 class WeatherViewModel: ObservableObject {
+
+    // MARK: - vars
+
+    let userDefaults = UserDefaults.standard
+    let notificationCenter = NotificationCenter.default
+    var locationManager = LocationManager()
+
+    let requestedDataWebSite = "https://api.weatherapi.com/v1/"
+    let key = "d0a9ecd662d7487b911111422221903"
+
+    enum UserDefaultsKeys: String {
+        case lastSavedLatitude, lastSavedLongitude
+    }
+
+    @Published var lastSavedLocation: CLLocationCoordinate2D?
 
     @Published var weather: Weather?
 
@@ -16,83 +32,119 @@ class WeatherViewModel: ObservableObject {
 
     @Published var lastMark: Mark?
 
+    @Published var isShowLoader = false
+
     var weatherUrl = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=d0a9ecd662d7487b911111422221903&q=London&days=10&aqi=no&alerts=no")
 
-    let notificationCenter = NotificationCenter.default
+    // MARK: - computed properties
 
-    var locationManager = LocationManager()
+    var requestForecastString: String {
+        return "\(requestedDataWebSite)forecast.json?key=\(key)&"
+    }
 
     var userLatitude: String {
         return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
     }
-
-    var userLatitudeDouble: Double {
-        return locationManager.lastLocation?.coordinate.latitude ?? 0
-    }
-
-    var userLongitudeDouble: Double {
-        return locationManager.lastLocation?.coordinate.longitude ?? 0
-    }
-
     var userLongitude: String {
         return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
     }
 
-    func changeLocation(text: String) {
-        self.weatherUrl = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=d0a9ecd662d7487b911111422221903&q=\(text)&days=10&aqi=no&alerts=no")
+    // MARK: - init
+
+    init() {
+        if let lastSavedLat = userDefaults.object(forKey: UserDefaultsKeys.lastSavedLatitude.rawValue) as? Double {
+            if let lastSavedLong = userDefaults.object(forKey: UserDefaultsKeys.lastSavedLongitude.rawValue) as? Double {
+                self.lastSavedLocation = CLLocationCoordinate2D(latitude: lastSavedLat, longitude: lastSavedLong)
+                self.weatherUrl = URL(string: "\(requestForecastString)&q=\(lastSavedLat),\(lastSavedLong)&days=10&aqi=no&alerts=no")
+            }
+        }
+    }
+
+    // MARK: - functions
+
+    func changeLocation(cityName: String) {
+        self.weatherUrl = URL(string: "\(requestForecastString)&q=\(cityName)&days=10&aqi=no&alerts=no")
         self.getWeather { _ in
         }
     }
 
-    func getWeather(competition: (Error?) -> Void) {
-        guard let weatherUrl else { return }
-        let task = URLSession.shared.dataTask(with: weatherUrl) { data, response, error in
-            guard let data, error == nil else { return }
-
-            DispatchQueue.main.async { [self] in
-
-                do {
-                    self.weather = try JSONDecoder().decode(Weather.self, from: data)
-                    self.notificationCenter.post(name: Notification.Name("receivedData"), object: nil)
-                } catch {
-                    print(error)
-                }
-            }
-
-        }
-        task.resume()
-    }
-
-    func getCurrentLocation() {
-        self.weatherUrl = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=d0a9ecd662d7487b911111422221903&q=\(userLatitude),\(userLongitude)&days=10&aqi=no&alerts=no")
-        getWeather { _ in }
-    }
-
     func changeLocation(location: CLLocationCoordinate2D) {
-        self.weatherUrl = URL(string: "https://api.weatherapi.com/v1/forecast.json?key=d0a9ecd662d7487b911111422221903&q=\(location.latitude),\(location.longitude)&days=10&aqi=no&alerts=no")
+        self.weatherUrl = URL(string: "\(requestForecastString)q=\(location.latitude),\(location.longitude)&days=10&aqi=no&alerts=no")
         lastMark = Mark(coordinate: location)
         getWeather { _ in }
     }
 
+    func switchToCurrentLocation(accessToLocationDenied: () -> Void) {
+        if locationManager.locationStatus == .authorizedAlways || locationManager.locationStatus == .authorizedWhenInUse {
+            self.weatherUrl = URL(string: "\(requestForecastString)&q=\(userLatitude),\(userLongitude)&days=10&aqi=no&alerts=no")
+            getWeather { _ in }
+        } else {
+            accessToLocationDenied()
+        }
+    }
     
-    func getLocations(text: String) {
-        guard let locationURL = URL(string: "https://api.weatherapi.com/v1/search.json?key=d0a9ecd662d7487b911111422221903&q=\(text)") else { return }
-        let task = URLSession.shared.dataTask(with: locationURL) { data, response, error in
-            guard let data, error == nil else { return }
+    func getPlacesList(text: String) {
+        guard let locationURL = URL(string: "\(requestedDataWebSite)search.json?key=\(key)&q=\(text)") else { return }
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+            let task = URLSession.shared.dataTask(with: locationURL) { data, response, error in
+                guard let data, error == nil else { return }
 
-            DispatchQueue.main.async { [self] in
-
-                do {
-                    self.locations = try JSONDecoder().decode([SearchLocation].self, from: data)
-                    self.notificationCenter.post(name: Notification.Name("receivedData"), object: nil)
-                } catch {
-                    print(error)
+                DispatchQueue.main.async {
+                    do {
+                        self?.locations = try JSONDecoder().decode([SearchLocation].self, from: data)
+                    } catch {
+                        print(error)
+                    }
                 }
             }
-
+            task.resume()
         }
-        task.resume()
     }
 
-    
+
+    func getWeather(competition: (Error?) -> Void) {
+        showLoader()
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+            guard let weatherUrl = self?.weatherUrl else { return }
+            let task = URLSession.shared.dataTask(with: weatherUrl) { data, response, error in
+                guard let data, error == nil else { return }
+                DispatchQueue.main.async {
+                    do {
+                        self?.weather = try JSONDecoder().decode(Weather.self, from: data)
+
+                        self?.lastSavedLocation = CLLocationCoordinate2D(latitude: self?.weather?.location.lat ?? 0,
+                                                                   longitude: self?.weather?.location.lon ?? 0)
+
+
+                        self?.saveCoordinates(coordinates: self?.lastSavedLocation ?? CLLocationCoordinate2D(latitude: 0,
+                                                                                                       longitude: 0))
+                        self?.notificationCenter.post(name: Notification.Name("receivedData"), object: nil)
+                        self?.hideLoader()
+                    } catch {
+                        print(error)
+                        self?.hideLoader()
+                    }
+                }
+
+            }
+            task.resume()
+        }
+    }
+
+    private func showLoader() {
+        withAnimation(.easeInOut) {
+            isShowLoader = true
+        }
+    }
+
+    private func hideLoader() {
+        withAnimation(.easeInOut) {
+            isShowLoader = false
+        }
+    }
+
+    func saveCoordinates(coordinates: CLLocationCoordinate2D) {
+        userDefaults.set(coordinates.latitude, forKey: UserDefaultsKeys.lastSavedLatitude.rawValue)
+        userDefaults.set(coordinates.longitude, forKey: UserDefaultsKeys.lastSavedLongitude.rawValue)
+    }
 }
